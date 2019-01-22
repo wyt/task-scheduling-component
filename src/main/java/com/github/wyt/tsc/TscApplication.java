a1d75bf40b5e77919d7d4d919c79853d2d114274
wangyongtao <wangyt4dev@gmail.com>
2019-01-22 10:43:32 +0800

    first commit

---
+++ src/main/java/com/github/wyt/tsc/TscApplication.java
@@ -0,0 +1,14 @@
+package com.github.wyt.tsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TscApplication.class, args);
+	}
+
+}
+
